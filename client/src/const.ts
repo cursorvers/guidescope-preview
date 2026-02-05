a91e898bd6be075ac373f3542ce8c1ee2a9255db
masayuki.O <masa.stage1@gmail.com>
2026-02-05 18:40:07 +0900

    Deploy: プライバシー警告・監査ログ機能追加

--- client/src/const.ts
+++
@@ -1,8 +0,0 @@
-/**
- * GuideScope - Constants
- * Note: This is a static site deployed to GitHub Pages.
- * Server-side constants are not used.
- */
-
-export const APP_NAME = 'GuideScope';
-export const APP_VERSION = '1.0.0';
